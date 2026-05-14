2e0d59751aaad93e16e4bf816e95aa9846a6cdf9
Elise Allen <eallen@op.ac.nz>
2026-05-15 10:53:56 +1200

    Add name prop to card component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -1,11 +1,11 @@
 <script>
-    let name = "Web 1";
+    let { name = 'Web 1' } = $props();
 </script>
 
 <p>{name} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint exercitationem voluptate veritatis explicabo temporibus. Non dicta, eos commodi temporibus voluptatibus voluptate ratione totam, inventore possimus, nesciunt sint explicabo officiis debitis?</p>
 
 <style>
     p {
-        color: hotpink; 
+        color: magenta; 
     }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script>
+    import Card from "$lib/components/Card.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+ 
+<Card name={"Elise"} />
+<Card name={"Buddy"} />
+<Card name={"Fundamentals of Web Development"} />
+<Card name={"ID512"} />
+<Card />
+<Card />
+<Card />
+<Card />
+<Card />
+<Card />
