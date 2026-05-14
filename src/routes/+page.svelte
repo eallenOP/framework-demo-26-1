bc19667bf4382b07551e82a613fc1365b0ccdbe0
Elise Allen <eallen@op.ac.nz>
2026-05-15 11:41:42 +1200

    Convert Bill Murray to an object

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,18 @@
 <script>
     import Card from "$lib/components/Card.svelte";
     import bill from "$lib/images/bill.jpeg";
+
+    let person = {
+        name: "Bill Murray",
+        image: bill
+    }
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
  
 <Card name={"Elise"} />
-<Card name={"Bill Murray"} image={bill} />
+<Card {...person} />
 <Card name={"Buddy"} />
 <Card name={"Fundamentals of Web Development"} />
 <Card name={"ID512"} image={"https://plus.unsplash.com/premium_photo-1666777247416-ee7a95235559?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} />
