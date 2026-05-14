8344ebf41c806b4da6578210a6b9a68da463efcd
Elise Allen <eallen@op.ac.nz>
2026-05-15 10:36:31 +1200

    My first component

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,11 @@
+<script>
+    let name = "Web 1";
+</script>
+
+<p>{name} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint exercitationem voluptate veritatis explicabo temporibus. Non dicta, eos commodi temporibus voluptatibus voluptate ratione totam, inventore possimus, nesciunt sint explicabo officiis debitis?</p>
+
+<style>
+    p {
+        color: hotpink; 
+    }
+</style>
