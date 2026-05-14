585760ff480771b880f7cf5e5a7dfe55af8e389a
Elise Allen <eallen@op.ac.nz>
2026-05-15 11:09:46 +1200

    Put an actual card in the card component

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -2,10 +2,26 @@
     let { name = 'Web 1' } = $props();
 </script>
 
-<p>{name} Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint exercitationem voluptate veritatis explicabo temporibus. Non dicta, eos commodi temporibus voluptatibus voluptate ratione totam, inventore possimus, nesciunt sint explicabo officiis debitis?</p>
+<div>
+    <img src="https://placehold.co/200" alt="Placeholder person">
+    <aside>
+        <h3>{name}</h3>
+        <ul>
+            <li>Hobby 1</li>
+            <li>Hobby 2</li>
+            <li>Hobby 3</li>
+        </ul>
+    </aside>
+</div>
 
 <style>
-    p {
-        color: magenta; 
+    div {
+        color: #333;
+        display: flex;
+        gap: 1em;
+        border: 1px solid #ddd;
+        margin-bottom: 1em;
+        padding: 1em;
+        max-width: 400px;
     }
 </style>
